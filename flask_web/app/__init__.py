55030bdae0ed2974c0589311760846654f1f2b5d
ZEROZhuLing <zero141104@gmail.com>
2017-10-27 16:42:53 +0800

    login

--- flask_web/app/__init__.py
+++ flask_web/app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_mail import Mail
+from flask_login import LoginManager
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 from config import config
@@ -9,6 +10,10 @@ bootstrap = Bootstrap()
 mail = Mail()
 moment = Moment()
 db = SQLAlchemy()
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'    #None,'basic','strong'代表防止用户会话被篡改的不同安全等级
+login_manager.login_view = 'auth.login'     #设置登录页面的端点,登录路由在蓝本中定义.所以要加上蓝本的名字
 
 
 def create_app(config_name):
@@ -20,9 +25,13 @@ def create_app(config_name):
     mail.init_app(app)
     moment.init_app(app)
     db.init_app(app)
+    login_manager.init_app(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix='/auth')
 
     return app
 
--- flask_web/app/models.py
+++ flask_web/app/models.py
@@ -1,5 +1,32 @@
-from . import db
+from . import db,login_manager
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash,check_password_hash
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class User(UserMixin,db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    email = db.Column(db.String(64),unique=True,index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    password_hash = db.Column(db.String(128))
+
+    @property
+    def password(self):
+        raise AttributeError('Password is not a readable attribute')
+
+    @password.setter
+    def password(self,password):   #计算密码散列值存在password_hash变量中
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self,password):     #验证密码是否正确，当前密码转换成的散列值与在库散列值是否相同
+        return check_password_hash(self.password_hash,password)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 class Role(db.Model):
     __tablename__ = 'roles'
@@ -9,13 +36,3 @@ class Role(db.Model):
 
     def __repr__(self):
         return '<Role %r>' % self.name
-
-
-class User(db.Model):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), unique=True, index=True)
-    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
-
-    def __repr__(self):
-        return '<User %r>' % self.username
