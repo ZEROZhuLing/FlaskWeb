ee46529ac61f467aef4538140b06d5fe8f7a29e0
ZEROZhuLing <zero141104@gmail.com>
2017-11-03 08:50:58 +0800

    update password

--- flask_web/app/auth/forms.py
+++ flask_web/app/auth/forms.py
@@ -1,16 +1,16 @@
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,BooleanField,SubmitField
 from wtforms.validators import DataRequired,Length,Email,Regexp,EqualTo
 from wtforms import ValidationError
 from ..models import User
 
-class LoginForm(Form):
+class LoginForm(FlaskForm):
     email = StringField('Email',validators=[DataRequired(),Length(1,64),Email()])
     password = PasswordField('Password',validators=[DataRequired()])
     remember_me = BooleanField('Keep me logged in')    #表示复选框
     submit = SubmitField('Log In')
 
-class RegisterForm(Form):
+class RegisterForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Length(1, 64), Email()])
     username = StringField('Username',validators=[DataRequired(),Length(1,64),Regexp('^[A-Za-z][A-Za-z0-9_.]*$',0,
                                                                                      'Usernames muset have only letters,'
@@ -27,3 +27,11 @@ class RegisterForm(Form):
     def validate_username(self,field):
         if User.query.filter_by(username=field.data).first():
             raise ValidationError('Username already in use.')
+
+
+class ChangePasswordForm(FlaskForm):
+    old_password = PasswordField('Old password', validators=[DataRequired()])
+    password = PasswordField('New password', validators=[
+        DataRequired(), EqualTo('password2', message='Passwords must match')])
+    password2 = PasswordField('Confirm new password', validators=[DataRequired()])
+    submit = SubmitField('Update Password')
--- flask_web/app/models.py
+++ flask_web/app/models.py
@@ -1,10 +1,9 @@
 from . import db,login_manager
 from flask_login import UserMixin
+from flask import current_app
 from werkzeug.security import generate_password_hash,check_password_hash
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
 
 class User(UserMixin,db.Model):
     __tablename__ = 'users'
@@ -13,6 +12,7 @@ class User(UserMixin,db.Model):
     email = db.Column(db.String(64),unique=True,index=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     password_hash = db.Column(db.String(128))
+    confirmed = db.Column(db.Boolean, default=False)
 
     @property
     def password(self):
@@ -25,6 +25,22 @@ class User(UserMixin,db.Model):
     def verify_password(self,password):     #验证密码是否正确，当前密码转换成的散列值与在库散列值是否相同
         return check_password_hash(self.password_hash,password)
 
+    def generate_confirmation_token(self, expiration=3600):
+        s = Serializer(current_app.config['SECRET_KEY'], expiration)
+        return s.dumps({'confirm': self.id})
+
+    def confirm(self, token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except:
+            return False
+        if data.get('confirm') != self.id:
+            return False
+        self.confirmed = True
+        db.session.add(self)
+        return True
+
     def __repr__(self):
         return '<User %r>' % self.username
 
@@ -36,3 +52,8 @@ class Role(db.Model):
 
     def __repr__(self):
         return '<Role %r>' % self.name
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
